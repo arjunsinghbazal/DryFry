b803f582a283bac6945e706bdf34e53f56c09433
Arjun Singh <arjunsinghbazal@gmail.com>
2023-09-01 02:18:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './home';
 import Checkout from './checkout'; 
 import ThankYou from './thank';
 function App() {
-<<<<<<< HEAD
+
   return (
     <Router>
       <Routes>
@@ -13,123 +13,7 @@ function App() {
         <Route path='/thank' element={<ThankYou/>}/>
       </Routes>
     </Router>
-=======
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-  const [value, setValue] = useState("");
-  const [data, setData] = useState([]);
-  const [filter, setFilter] = useState([]);
-  const [activeButton, setActiveButton] = useState("all");
-  const nonVegCategories = ['Chicken', 'Seafood', 'Vegan', 'Goat','Lamb','Pork'];
-  const VegCategories = ["Pasta","Starter","Dessert","Vegetarian"];
-  const fetchData = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(url);
-      const responseData = await response.json();
-      const categories = responseData.categories;
-      setData(categories);
-      setFilter(categories);
-      console.log(data)
-      console.log(filter)
-    } catch (error) {
-      setError("Unable to fectch the data");
-    } finally {
-      setLoading(false);
-    }
-  };
 
-  const handleVegClick=()=>{
-    const filterVegData=data.filter(items=>VegCategories.includes(items.strCategory))
-    setFilter(filterVegData)
-  }
-
-  const handleNonVegClick = () => {
-    const filteredNonVegData = data.filter(category =>
-      nonVegCategories.includes(category.strCategory)
-    );
-    setFilter(filteredNonVegData);
-  };
-
-  const handleSearch = () => {
-    if (value !== "") {
-      const searchFilter = data.filter((items) =>
-        items.strCategory.toLowerCase().includes(value.toLowerCase())
-      );
-      setFilter(searchFilter);
-      setActiveButton("all")
-    } else {
-      showAllItems();
-    }
-  };
-  const showAllItems = () => {
-    setFilter(data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-
-
-
-  if (loading) return <div className="loader"></div>;
-
-  return (
-    <div className="Container">
-      <section className="TopContainer">
-      <div className="loogo">
-  <h1>
-    <img src={logo} alt="logo" />
-    D<span className="green">r</span>y F<span className="green">r</span>y
-  </h1>
-</div>
-        <div className="search">
-          <input
-            placeholder="Search here"
-            value={value}
-            onChange={(e) => {
-            setValue(e.target.value);
-            handleSearch(); 
-          }}
-          />
-        </div>
-      </section>
-      <section className="FilterContainer">
-      <button
-  className={`btn ${activeButton === "all" ? "active" : ""}`}
-  onClick={() => {
-    showAllItems();
-    setActiveButton("all");
-  }}
->
-  All items
-</button>
-
-<button
-  className={`btn ${activeButton === "nonveg" ? "active" : ""}`}
-  onClick={() => {
-    handleNonVegClick();
-    setActiveButton("nonveg");
-  }}
->
-  Non Vegetarian
-</button>
-
-<button
-  className={`btn ${activeButton === "veg" ? "active" : ""}`}
-  onClick={() => {
-    handleVegClick();
-    setActiveButton("veg");
-  }}
->
-  Vegetarian
-</button>
-
-      </section>
-      <Cart data={filter} error={error}/>
-    </div>
->>>>>>> a91178f7fed4e3cc6dbb4201dc16f234d57dae44
   );
 }
 
--- src/Cart.js
+++ src/Cart.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 
 const Cart = ({ data, error }) => {
   const prices = [120, 230, 330, 430, 540, 299, 199, 600, 499, 100, 444, 33, 222, 523, 287];
-<<<<<<< HEAD
 const [Data,setData]=useState([]);
 
   const setOrder = (index) => {
@@ -13,9 +12,6 @@ const [Data,setData]=useState([]);
 useEffect(()=>{
   localStorage.setItem("orders", JSON.stringify(Data));
 },[Data])
-=======
-  
->>>>>>> a91178f7fed4e3cc6dbb4201dc16f234d57dae44
   return (
     <section className="FoodCardContainer">
       <div className="FoodCards">
@@ -27,12 +23,12 @@ useEffect(()=>{
                 <div className="food_info">
                   <h3>{value.strCategory}</h3>
                   <p>{value.strCategoryDescription}</p>
-<<<<<<< HEAD
+
                  <div> <button className="btn">Price {prices[value.idCategory]} INR</button>
                   <button className="btn" onClick={()=>setOrder(value.idCategory)}>Order</button></div>
-=======
+
                   <button className="btn">Price {prices[value.idCategory]} INR</button>
->>>>>>> a91178f7fed4e3cc6dbb4201dc16f234d57dae44
+
                 </div>
               </div>
             )
